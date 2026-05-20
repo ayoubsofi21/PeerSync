c087049d7086b1f7cbcee7adfd785871d81da6c0
ayoubsofi <ayoub@email.com>
2026-05-20 11:46:27 +0100

    add PDO database connection configuration

---
+++ config/Database.php
@@ -0,0 +1,16 @@
+<?php
+
+class Database
+{
+    public static function connect(): PDO
+    {
+        try {
+            $pdo = new PDO("mysql:host=localhost;dbname=peersync","root","");
+            echo "Database connected successfully";
+            return $pdo;
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+        }
+        }
+$db=Database::connect();

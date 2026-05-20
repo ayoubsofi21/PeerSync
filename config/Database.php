8db5316aef1553035d1f5c016bedc42a45fd91e0
ayoubsofi <ayoub@email.com>
2026-05-20 12:41:46 +0100

    update the database

--- config/Database.php
+++ config/Database.php
@@ -6,11 +6,11 @@ class Database
     {
         try {
             $pdo = new PDO("mysql:host=localhost;dbname=peersync","root","");
-            echo "Database connected successfully";
+            // echo "Database connected successfully";
             return $pdo;
         } catch (PDOException $e) {
             die("Connection failed: " . $e->getMessage());
         }
         }
         }
-$db=Database::connect();
+// $db=Database::connect();

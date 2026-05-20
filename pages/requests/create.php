97d6fa96b7a8ccf4edcc3e46a3afeabe6885b324
ayoubsofi <ayoub@email.com>
2026-05-20 11:53:17 +0100

    add help request creation form

---
+++ pages/requests/create.php
@@ -0,0 +1,10 @@
+<form action="../../actions/create-request.php" method="POST">
+
+<input name="title" placeholder="Title">
+<input name="description" placeholder="Description">
+<input name="technology" placeholder="Technology">
+<input name="student_id" placeholder="Student ID">
+
+<button>Create</button>
+
+</form>
